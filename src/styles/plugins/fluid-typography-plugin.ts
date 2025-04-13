459bd20b9a99f7add30b8470f66876a6d5f707eb
rhymbit <rhymbit@gmail.com>
2025-04-13 21:35:35 +0530

    Refactored styling for blog pages

--- src/styles/plugins/fluid-typography-plugin.ts
+++ src/styles/plugins/fluid-typography-plugin.ts
@@ -3,53 +3,53 @@ import plugin from "tailwindcss/plugin";
 export default plugin(function ({ addBase, addComponents }) {
   addBase({
     html: {
-      "font-size": "clamp(1rem, calc(0.5rem + 0.5vw), 1.4rem);",
+      "font-size": "clamp(1rem, calc(0.3rem + 0.3vw), 1.2rem);",
 
       "h1,h2,h3,h4,h5,h6": {
         color:
           "color-mix(in srgb, var(--color-base-content) 93%, var(--color-primary));",
       },
       h1: {
-        "font-size": "clamp(3rem, calc(2.5rem + 1.9vw), 9rem);",
+        "font-size": "clamp(2.5rem, calc(2.2rem + 1.7vw), 7rem);"
       },
       h2: {
-        "font-size": "clamp(2.5rem, calc(2.2rem + 1.7vw), 7rem);",
-      },
-      h3: {
         "font-size": "clamp(2rem, calc(1.7rem + 1.5vw), 6.5rem);",
       },
-      h4: {
+      h3: {
         "font-size": "clamp(2rem, calc(1.8rem + 1vw), 6rem);",
       },
-      h5: {
+      h4: {
         "font-size": "clamp(1.5rem, calc(1.3rem + 1vw), 5.5rem);",
       },
-      h6: {
+      h5: {
         "font-size": "clamp(1rem, calc(0.8rem + 1vw), 5rem);",
+      },
+      h6: {
+        "font-size": "clamp(1rem, calc(0.6rem + 0.8vw), 4rem);",
       },
     },
   }),
     addComponents({
       ".text-h1": {
-        "font-size": "clamp(3rem, calc(2.5rem + 1.9vw), 9rem);",
-      },
-      ".text-h2": {
         "font-size": "clamp(2.5rem, calc(2.2rem + 1.7vw), 7rem);",
       },
-      ".text-h3": {
+      ".text-h2": {
         "font-size": "clamp(2rem, calc(1.7rem + 1.5vw), 6.5rem);",
       },
-      ".text-h4": {
+      ".text-h3": {
         "font-size": "clamp(2rem, calc(1.8rem + 1vw), 6rem);",
       },
-      ".text-h5": {
+      ".text-h4": {
         "font-size": "clamp(1.5rem, calc(1.3rem + 1vw), 5.5rem);",
       },
-      ".text-h6": {
+      ".text-h5": {
         "font-size": "clamp(1rem, calc(0.8rem + 1vw), 5rem);",
       },
+      ".text-h6": {
+        "font-size": "clamp(1rem, calc(0.6rem + 0.8vw), 4rem);",
+      },
       ".text-p": {
-        "font-size": "clamp(1rem, calc(0.5rem + 0.7vw), 1.4rem);",
+        "font-size": "clamp(1rem, calc(0.3rem + 0.3vw), 1.2rem);",
       },
     });
 });
